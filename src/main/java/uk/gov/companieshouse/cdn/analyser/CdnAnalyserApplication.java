b9f022475649882adb0a5327b8b6e10e53a03a0c
Dan Toulcher <dtoulcher@companieshouse.gov.uk>
2025-01-21 14:40:17 +0000

    feat: updated to reflect review comments
    CC-1628

--- src/main/java/uk/gov/companieshouse/cdn/analyser/CdnAnalyserApplication.java
+++ src/main/java/uk/gov/companieshouse/cdn/analyser/CdnAnalyserApplication.java
@@ -1,14 +1,10 @@
 package uk.gov.companieshouse.cdn.analyser;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CdnAnalyserApplication {
-
-    @Value("${spring.application.name}")
-    public static String applicationNameSpace;
 
     public static void main(String[] args) {
         SpringApplication.run(CdnAnalyserApplication.class, args);
